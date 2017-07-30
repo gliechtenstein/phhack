873391586b659405ae3eab07bc0d952e69166fc9
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-07-30 17:39:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var run = function() {
   var main = document.body.querySelector("main");
   if(main) {
     var primary = document.body.querySelector('[class^="primaryContent"]');
-    main.appendTo(primary);
+    main.appendChild(primary);
     main.children.forEach(function(c) {
       if(/primaryContent/g.test(c.className)) {
       } else {
